a7bcb0bcd77435935aac828f639d473c183df23d
jahidunfarabi <jahidunmuntaka25@gmail.com>
2026-01-17 04:53:33 +0600

    Database connection

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+// Database configuration
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'campus_connect_db');
+
+// App Root (Full path to the 'app' folder)
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL Root (The link to your website)
+define('URLROOT', 'http://localhost/campus_connect');
+
+// Site Name
+define('SITENAME', 'Campus Connect');
